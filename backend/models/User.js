dfe2978e290d2bdf5cd528199e4da14b25156cef
Ksaikiran28 <ksaikiran1469@gmail.com>
2025-08-27 00:03:43 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    email : {type:String,required:true,unqiue:true},
+    email : {type:String,required:true,unique:true},
     fullName:{type:String,required:true},
     password:{type:String,required:true,minlength:6},
     profilePic:{type:String,default:""},
